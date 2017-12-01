f187ec0fc9801e293602e7d314a7ccb6ca8de71b
Imenghrs <imengharsalli123@gmail.com>
2017-12-01 20:19:57 +0100

    Rollback

--- TenderCare/src/main/java/com/tendercare/dao/cassandra/ICareServiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tendercare.dao.cassandra;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tendercare.model.h2.CareService;
-
-public interface ICareServiceRepository extends JpaRepository<CareService, Long> {
-}
